7e32f6df80f5ab9ab3e4a9f3e0195c5e118dc3a4
Alex <alexplusha@gmail.com>
2018-11-12 21:03:39 +0300

    Add some

--- src/main/java/com/newsweaterproject/controller/RegistrationController.java
+++ src/main/java/com/newsweaterproject/controller/RegistrationController.java
@@ -1,13 +1,20 @@
 package com.newsweaterproject.controller;
 
+import com.newsweaterproject.domain.Role;
 import com.newsweaterproject.domain.User;
+import com.newsweaterproject.repos.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.Collections;
+import java.util.Map;
+
 @Controller //спринг через МэйнСервлет будет передавать запросы именно этому контролерру при востребовании
 public class RegistrationController {
-
+    @Autowired
+    private UserRepo userRepo; // сприг кидает инЪекцию))
     @GetMapping("/registration")
     public String registration() {
 
@@ -16,10 +23,20 @@ public class RegistrationController {
     }
 
     @PostMapping("/registration") //маппинг для гет зпросов
-    public String addUser(User user){//для поиска пользователя необходимо создать репозитроий UserRepo
+    public String addUser(User user, Map<String,Object> model){//для поиска пользователя необходимо создать репозитроий UserRepo
 
+        User userFromDb = userRepo.findByUsername(user.getUsername());//получаем юзера из базы
+        if (userFromDb!=null){
+            model.put("message","User exist!");
+            return "registration";  //если пользователь есть то возвращем вью регистрации
 
-        return "/redirect:/login";//при успешной авторизации редирект на форму логина
+        }
+        //Если пользователя нет
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));//создание сета с одним значением
+        userRepo.save(user);//сохранение пользователя
+
+        return "redirect:/login";//при успешной авторизации редирект на форму логина
     }
 
 }
