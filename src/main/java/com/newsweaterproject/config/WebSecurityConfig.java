5175fff1c908dd92a299822118e2e4275076da88
Alex <alexplusha@gmail.com>
2018-11-12 21:11:23 +0300

    Add some

--- src/main/java/com/newsweaterproject/config/WebSecurityConfig.java
+++ src/main/java/com/newsweaterproject/config/WebSecurityConfig.java
@@ -38,7 +38,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 dataSource(dataSource)
                 .passwordEncoder(NoOpPasswordEncoder.getInstance())
                 .usersByUsernameQuery("select username, password, active from usr where username=?")
-        .authoritiesByUsernameQuery("select u.username ur.roles from usr u inner join user_role ur" +
+        .authoritiesByUsernameQuery("select u.username ur.roles from usr u inner join user_role ur " +
                 "on u.id = ur.user_id where u.username=?");
         //Запрос для того чтобы система могла найти пользователя по его имени
 
