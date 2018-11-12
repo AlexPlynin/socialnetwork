3673424383f353b1a808faf54e882eb45aa78301
Alex <alexplusha@gmail.com>
2018-11-12 19:33:44 +0300

    add Spring Sec dependency

--- src/main/java/com/newsweaterproject/GreetingController.java
+++ src/main/java/com/newsweaterproject/GreetingController.java
@@ -16,13 +16,13 @@ public class GreetingController {
     @Autowired
     private MessageRepos messageRepos;
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Map<String, Object> model) {
-        model.put("name", name);
+    @GetMapping("/")
+    public String greeting(Map<String, Object> model) {
+
         return "greeting";
     }
 
-    @GetMapping
+    @GetMapping("/main")
     public String main(Map<String, Object> model) {
         Iterable<Message> messages = messageRepos.findAll();            //получаем данные с репозитория
         model.put("messages", messages);                                //пробрасываем в вид
@@ -31,7 +31,7 @@ public class GreetingController {
         return "main";
     }
 
-    @PostMapping("apply")
+    @PostMapping("/main")
     public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
 
         Message message = new Message(text, tag);
